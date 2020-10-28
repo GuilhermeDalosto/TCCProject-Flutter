0eec5e48ba3c3c272dda44c382fb50a31309b17c
Caio Azevedo <caioba19@gmail.com>
2020-10-28 10:03:29 -0300

    Added MemberListView

---
+++ caregiverproject/lib/MemberListView.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Member {
+  String name;
+  bool isAdm;
+
+  Member(String name, bool isAdm) {
+    this.name = name;
+    this.isAdm = isAdm;
+  }
+}
+
+class MemberList extends StatefulWidget {
+  @override
+  State createState() => new DyanmicList();
+}
+
+class DyanmicList extends State<MemberList> {
+  List<Member> members = [
+    Member("Caio", false),
+    Member("Fabio", false),
+    Member("Guilherme", true)
+  ];
+
+  Widget _getAdmin(BuildContext context, int index) {
+    return ListTile(
+      title: Text(members[index].name),
+      trailing: members[index].isAdm ? Icon(Icons.star) : Icon(Icons.person),
+    );
+  }
+
+  @override
+  Widget build(BuildContext ctxt) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Members")),
+      body: new ListView.separated(
+        separatorBuilder: (context, index) => Divider(
+          color: Colors.blueGrey[200],
+        ),
+        itemCount: members.length,
+        itemBuilder: _getAdmin,
+      ),
+    );
+  }
+}
